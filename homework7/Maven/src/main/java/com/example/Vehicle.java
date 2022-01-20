3d18b3660c898fd8f3360f0f3f8cd5f512869445
Shantanu Mukherjee <shantanu.mukherjee@kickdrumtech.com>
2022-01-20 18:22:29 +0530

    homework 7

---
+++ homework7/Maven/src/main/java/com/example/Vehicle.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public interface Vehicle {
+    public void drive();
+    public String getModel();
+    public void init();
+    public String getMileage();
+    public void destroy();
+
+}
