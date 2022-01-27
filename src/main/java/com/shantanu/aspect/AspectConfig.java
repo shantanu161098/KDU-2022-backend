15db587aaac5da61f635db95b6bacf3914df833c
Shantanu Mukherjee <shantanu.mukherjee@kickdrumtech.com>
2022-01-27 12:41:53 +0530

    homework 9

--- src/main/java/com/shantanu/aspect/AspectConfig.java
+++ src/main/java/com/shantanu/aspect/AspectConfig.java
@@ -17,61 +17,52 @@ public class AspectConfig {
      * Implementation of within Pointcut
      */
     @Pointcut("within(com.shantanu.aspect.operations.StringOperations)")
-    public void stringMethod() {}
+    public void stringMethod() {
+    }
 
     /**
      * Implementation of args method
      */
 
     @Pointcut("execution(public String *(int))")
-    public void stringWithArgs() {}
+    public void stringWithArgs() {
+    }
 
     @Before("stringWithArgs()")
-    public void beforeStringWithArgs(JoinPoint joinPoint){
+    public void beforeStringWithArgs(JoinPoint joinPoint) {
         System.out.println("Before arg " + joinPoint.getSignature().getName());
     }
 
     @Around("stringWithArgs()")
-    public String aroundStringWithArgs(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+    public String aroundStringWithArgs(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("Around arg");
         Object result = proceedingJoinPoint.proceed();
         return String.valueOf(result);
     }
 
     @After("stringWithArgs()")
-    public void afterStringWithArgs(JoinPoint joinPoint){
+    public void afterStringWithArgs(JoinPoint joinPoint) {
         System.out.println("After arg " + joinPoint.getSignature().getName());
     }
 
-    @AfterReturning(pointcut = "stringWithArgs()",returning = "result")
-    public void afterReturnStringWithArgs(String result){
+    @AfterReturning(pointcut = "stringWithArgs()", returning = "result")
+    public void afterReturnStringWithArgs(String result) {
         System.out.println(result);
     }
 
-    @AfterThrowing(pointcut = "stringWithArgs()",throwing = "error")
-    public void afterThrowingStringWithArgs(JoinPoint joinPoint,Throwable error){
+    @AfterThrowing(pointcut = "stringWithArgs()", throwing = "error")
+    public void afterThrowingStringWithArgs(JoinPoint joinPoint, Throwable error) {
         System.out.println(error.getMessage());
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
     @Before("stringMethod()")
-    public void beforeStringConcat(JoinPoint joinPoint){
-        System.out.println("Before : "+ joinPoint.getSignature().getName());
+    public void beforeStringConcat(JoinPoint joinPoint) {
+        System.out.println("Before : " + joinPoint.getSignature().getName());
     }
 
     @Around("stringMethod()")
-    public String concatAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+    public String concatAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("Around advice of string method ");
         Object result = proceedingJoinPoint.proceed();
         return String.valueOf(result);
@@ -92,17 +83,17 @@ public class AspectConfig {
         System.out.println("After Return " + result);
     }
 
-   @Around("anyMethod()")
+    @Around("anyMethod()")
     public Double around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("Around advice");
         Object result = proceedingJoinPoint.proceed();
         System.out.println("Around advice returned  " + result);
         return Double.valueOf(result.toString());
-   }
+    }
 
-   @AfterThrowing(pointcut = "anyMethod()",throwing = "error")
-    public void afterThrowing(JoinPoint joinpoint,Throwable error){
+    @AfterThrowing(pointcut = "anyMethod()", throwing = "error")
+    public void afterThrowing(JoinPoint joinpoint, Throwable error) {
         System.out.println("In after throwing advice " + error.getMessage());
-   }
+    }
 
 }
